fcf2d6a0c9bbfdb52595c269fb2dd57292b5825e
rootspyro <root.spyro@gmail.com>
2024-12-08 21:04:14 -0400

    feat: add build.sh script

---
+++ build.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+#
+source .env
+
+binName=gonesis
+builds=dist
+version=$APP_VERSION
+checksumsName=$binName\_$version\_checksums.txt
+
+
+BuildBinary() {
+  # $1 = arch
+  # $2 = os 
+
+  GCO_ENABLED=0 GOARCH=$1 GOOS=$2 go build  -o $binName cmd/$binName/main.go
+  local tarName=$binName\_$2\_$1.tar.gz
+
+  tar -czvf $tarName $binName
+  sha256sum $tarName >> $builds/$checksumsName
+  mv $tarName $builds/$tarName && rm $binName
+}
+
+# BUILD'S DIR
+if [ -d $builds ]; then
+  rm -r $builds
+fi
+
+# OS CONFIGURATION
+
+osList=("linux" "windows" "darwin")
+
+linuxArchs=("arm64" "386" "amd64")
+windowsArchs=("386" "amd64")
+macOsArchs=("amd64")
+
+declare -A os_archs
+
+os_archs["linux"]=${linuxArchs[@]}
+os_archs["windows"]=${windowsArchs[@]}
+os_archs["darwin"]=${macOsArchs[@]}
+
+# BUILD THE BINARIES
+
+mkdir $builds
+
+# checksums - SHA256
+touch $builds/$checksumsName
+
+for os in ${osList[@]}
+do
+  osArchs="${os_archs[$os]}"
+
+  for arch in ${osArchs[@]} 
+  do
+    BuildBinary $arch $os
+  done
+done
